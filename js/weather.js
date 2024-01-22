93deb7ac30a47463ec1beefea4b4a1b50e916fe1
Andre M. de Souza <andresouza44@gmail.com>
2024-01-21 21:17:31 -0300

    getCityUrl fetch

--- js/weather.js
+++ js/weather.js
@@ -1,1 +1,24 @@
-const apiKey = 'DqfDMyzYGFvWD5PjuyobqS74fd3fiM3C'
+const APIKey = 'PPL7RxHq6vXHiRHgoGGkywY9SnoHGP5L'
+
+
+const getCityUrl = cityName => `http://dataservice.accuweather.com/locations/v1/cities/search?apikey=${APIKey}&q=${cityName}`
+
+const getCityDate = async cityName =>{
+
+    try {
+        const cityUrl = getCityUrl(cityName)
+        const response = await fetch(cityUrl)
+        if(!response.ok){
+            throw new Error ('Não foi possível obter os dados')
+        }
+        const [cityData] = await response.json()
+        
+        return cityData
+    }catch({name, message}){
+        alert(`${name}: ${message}`)
+
+    }
+}
+
+
+console.log(getCityDate('PAra'))
